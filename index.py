a84aa557638958e69239bfe3821fa674c01a478a
farukh <farukh022198@gmail.com>
2024-02-23 00:29:42 -0500

    string manipulation on python

--- index.py
+++ index.py
@@ -1,1 +1,7 @@
-print('Hello World!')
+
+
+msg='welcome to Python 101: Strings'
+new_msg = msg[18]+' ' + msg[0:7]+' '+ msg[25:29]+' '+ msg[8:10]+' '+'Tyler'
+#print(msg[18],msg[0:7],msg[25:29], msg[8:10], 'Tyler')
+print(new_msg.title())
+print(new_msg[::-1].title())
