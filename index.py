6d472ddb870c76644b3ea6f374c0a65d13057085
farukh <farukh022198@gmail.com>
2024-02-24 16:03:40 -0500

    learned lists in python

--- index.py
+++ index.py
@@ -1,7 +1,19 @@
+sales_w1 = [7,3,42,19,15,35,9]
+sales_w2 = [12,4,26,10,7,28]
+sales = []
 
+w2_day7 = input('Please input number of lemonade sold: ')
 
-msg='welcome to Python 101: Strings'
-new_msg = msg[18]+' ' + msg[0:7]+' '+ msg[25:29]+' '+ msg[8:10]+' '+'Tyler'
-#print(msg[18],msg[0:7],msg[25:29], msg[8:10], 'Tyler')
-print(new_msg.title())
-print(new_msg[::-1].title())
+sales_w2.append(int(w2_day7)); 
+
+sales_w1.extend(sales_w2)
+
+sales = list(sales_w1)
+
+max_earned = max(sales)
+min_earned = min(sales)
+total_earned = sum(sales)
+print(max_earned)
+print(min_earned)
+print(total_earned) 
+
