3ee7beb39afae11d13bf53c36897584b7c71ec44
farukh <farukh022198@gmail.com>
2024-03-19 09:19:07 -0400

    crypto project

--- index.py
+++ index.py
@@ -1,7 +1,21 @@
-# Dictionary comprehensions
-movies = ["And Now for Something Completely Different","Monty Python and the Holy Grail",
-"Monty Python's Life of Brian","Monty Python Live at the Hollywood Bowl","Monty Python's The Meaning of Life","Monty Python Live (Mostly)"]
-year =[1971,1975,1979,1982,1983,2014]
-names = ['John','Eric','Michael','Graham','Terry','TerryG']
-
-print(movies + year)
+print('Project -  Crypto')
+def enigma_light():
+# create keys string
+    keys = 'abcdefghijklmnopqrstuvwxyz !'
+# autogenerate the values string by offsetting original string
+    values = keys[-1] + keys[0:-1]
+# create two dictionaries
+    dict_e = dict(zip (keys, values)) 
+    dict_d = dict(zip(values, keys))
+#user input 'the message' and mode
+    msg = input('Please enter a meassage')
+    mode = input('Please enter e for encrypt and d for decrypt')
+# run encode or decode
+    if mode == 'e':
+        new_msg = ''.join([dict_e[letter] for letter in msg.lower()])
+    else: 
+        new_msg = ''.join([dict_d[letter] for letter in msg.lower()])    
+# return result
+    return new_msg
+# clean and beautify the code 
+print(enigma_light())
